6e9e1f4de15272d823bf6ba29017675c3df18d94
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-08-23 13:48:51 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5855,7 +5855,7 @@ class PlayState extends MusicBeatState
 	{
 		if(SONG.dodgeEnabled){
 			// FlxG.keys.justPressed.SPACE
-			if(FlxG.keys.anyJustPressed(dodgeKey) && !bfDodging && bfCanDodge){
+			if(controls.ACCEPT(dodgeKey) && !bfDodging && bfCanDodge){
 				bfDodge();
 			}
 		}
